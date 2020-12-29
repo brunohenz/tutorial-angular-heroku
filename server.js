32fbd1ba3248c0787f9f18bd09dc10cd1ec81f56
Bruno Henz <brunohzsalomao@gmail.com>
2020-12-29 13:54:25 -0300

    Alteração de nome do diretorio dist

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-tutorial-heroku'));
+app.use(express.static('./dist/app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-tutorial-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
 );
 
 // Start the app by listening on the default Heroku port
